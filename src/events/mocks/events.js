2b127495a19c4ef9b64fd5b339ea469b6d8e6f46
Ricardo Montes <ricardo.montes@tdea.edu.co>
2024-10-18 14:03:34 -0500

    redisign events page

---
+++ src/events/mocks/events.js
@@ -0,0 +1,26 @@
+export const events = [
+  {
+    id: 1,
+    name: "Concierto de Rock",
+    date: "2024-11-01",
+    imageUrl: "https://picsum.photos/200/150?random=1",
+  },
+  {
+    id: 2,
+    name: "Festival de Cine",
+    date: "2024-11-15",
+    imageUrl: "https://picsum.photos/200/150?random=2",
+  },
+  {
+    id: 3,
+    name: "Carrera 10K",
+    date: "2024-12-05",
+    imageUrl: "https://picsum.photos/200/150?random=3",
+  },
+  {
+    id: 4,
+    name: "Feria de Tecnología",
+    date: "2025-01-20",
+    imageUrl: "https://picsum.photos/200/150?random=4",
+  },
+];
--- src/events/pages/EventsPage.jsx
+++ src/events/pages/EventsPage.jsx
@@ -1,7 +1,26 @@
+import { events } from "../mocks/events";
+
 export const EventsPage = () => {
   return (
-    <>
-      <h1>Events</h1>
-    </>
-  )
-}
+    <div className="container mt-4">
+      <h1 className="text-center mb-4">Eventos</h1>
+      <div className="row">
+        {events.map((event) => (
+          <div className="col-md-6 col-lg-4 mb-4" key={event.id}>
+            <div className="card">
+              <img
+                src={event.imageUrl}
+                className="card-img-top"
+                alt={`Imagen del evento ${event.name}`}
+              />
+              <div className="card-body">
+                <h5 className="card-title">{event.name}</h5>
+                <p className="card-text">Fecha: {event.date}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
